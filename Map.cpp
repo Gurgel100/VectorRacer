4f809fa7628ad55296017c238138dc8618069aa6
Pascal Störzbach <baesae@hotmail.ch>
2017-12-08 12:44:02 +0100

    Added functionality to print the map

--- Map.cpp
+++ Map.cpp
@@ -1,9 +1,28 @@
 #include "Map.h"
+#include <iostream>
+#include <string>
 
-Map::Map()
+using namespace std;
+
+Map::Map(size_t width, size_t height) : width(width), height(height)
 {
 }
 
 Map::~Map()
 {
+}
+
+void Map::print() const
+{
+	const string verticalDelimiter(width, '-');
+
+	cout << verticalDelimiter << endl;
+	for (size_t i = 0; i < height; i++)
+	{
+		for (size_t j = 0; j < width; j++)
+		{
+		}
+		cout << endl;
+	}
+	cout << verticalDelimiter << endl;
 }
--- Map.h
+++ Map.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <vector>
 #include "MapEntity.h"
+#include <vector>
 
 class Map
 {
 public:
-	Map();
+	Map(size_t width, size_t height);
 	~Map();
 
+	void print() const;
+
 private:
+	const size_t width, height;
 	std::vector<MapEntity> entities;
 };
 
