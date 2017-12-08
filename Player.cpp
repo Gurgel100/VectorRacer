5a7af2c00be7a256a91d5a49105030b6788c3f2d
Pascal Störzbach <baesae@hotmail.ch>
2017-12-08 12:43:22 +0100

    Added some functionality to Player class

--- Player.cpp
+++ Player.cpp
@@ -1,9 +1,18 @@
 #include "Player.h"
 
-Player::Player()
+using std::string;
+using Eigen::Vector2i;
+
+Player::Player(const string &name) : name(name)
 {
 }
 
 Player::~Player()
 {
+}
+
+void Player::move(const Vector2i &dir)
+{
+	this->dir += dir;
+	pos += this->dir;
 }
--- Player.h
+++ Player.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "Eigen\Eigen"
+#include <string>
+
 class Player
 {
 public:
-	Player();
+	Player(const std::string &name);
 	~Player();
+
+	void move(const Eigen::Vector2i &dir);
+
+private:
+	const std::string name;
+	Eigen::Vector2i pos;
+	Eigen::Vector2i dir;
 };
 
