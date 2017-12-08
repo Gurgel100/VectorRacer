9e1aa75b23232dd69024fe85b38c0bb4c3602b79
dariomo <dariomo@ethz.ch>
2017-12-08 11:31:22 +0100

    Added TrackReader class

---
+++ TrackReader.cpp
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TrackReader.cpp
+ * Author: dario
+ * 
+ * Created on December 8, 2017, 11:28 AM
+ */
+
+#include "TrackReader.h"
+#include <iostream>
+#include <fstream>
+
+TrackReader::TrackReader()
+{
+    
+}
+
+TrackReader::TrackReader(const TrackReader& orig)
+{
+}
+
+TrackReader::~TrackReader()
+{
+}
+
+void read_track(char* name)
+{
+    
+}
---
+++ TrackReader.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TrackReader.h
+ * Author: dario
+ *
+ * Created on December 8, 2017, 11:28 AM
+ */
+
+#ifndef TRACKREADER_H
+#define TRACKREADER_H
+
+class TrackReader
+{
+public:
+    TrackReader();
+    TrackReader(const TrackReader& orig);
+    virtual ~TrackReader();
+private:
+
+};
+
+#endif /* TRACKREADER_H */
+
